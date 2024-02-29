de0d32be93f9d5cd16c8dbcfcb8520f4f6f6b97f
Douglasjose3 <douglasjose3@gmail.com>
2024-02-29 15:19:05 -0800

    exercicio resolvido

--- exercicio_es6_4/dist/exercicio_es6.js
+++ exercicio_es6_4/dist/exercicio_es6.js
@@ -1,23 +1,48 @@
 "use strict";
 
-var alunos1 = ['Murilo', 'André', 'Luciana', 'Gustavo'];
-var alunos5ano = alunos1.map(function (nomesENotas) {
-  nomesENotas = {
-    nome: nomesENotas,
-    nota: 5
-  };
-  return nomesENotas;
-});
-console.log(alunos5ano);
-alunos5ano.push({
-  nome: 'Douglas',
+var alunos = [{
+  nome: 'Murilo',
+  nota: 5
+}, {
+  nome: 'André',
+  nota: 6
+}, {
+  nome: 'Luciana',
+  nota: 7
+}, {
+  nome: 'Gustavo',
   nota: 8
+}];
+alunos.push({
+  nome: 'Douglas',
+  nota: 10
 });
-var notaMaiorQ6 = alunos5ano.find(function (nota) {
-  return nota.nota == 8;
+var alunosNotas = alunos.filter(function (alunos) {
+  return alunos.nota >= 7;
 });
-console.log(notaMaiorQ6);
-var nota8 = alunos5ano.filter(function (achandoNota8) {
-  return achandoNota8.nota === 8;
-});
-console.log(nota8);
+console.log(alunosNotas);
+
+// const alunos1 = ['Murilo', 'André', 'Luciana', 'Gustavo'];
+// const alunos5ano = alunos1.map(function(nomesENotas) {
+//     nomesENotas = {
+//         nome: nomesENotas,
+//         nota: 5
+//     }
+//     return nomesENotas;
+// })
+// console.log(alunos5ano);
+
+// alunos5ano.push({
+//     nome: 'Douglas',
+//     nota: 8
+// })
+
+// const notaMaiorQ6 = alunos5ano.find(function(nota) {
+//     return nota.nota >= 8
+// })
+// console.log(notaMaiorQ6);
+
+// const nota8 = alunos5ano.filter(function(achandoNota8) {
+//     return achandoNota8.nota == 8
+// })
+// console.log(nota8);
--- exercicio_es6_4/src/exercicio_es6.js
+++ exercicio_es6_4/src/exercicio_es6.js
@@ -1,24 +1,38 @@
-const alunos1 = ['Murilo', 'André', 'Luciana', 'Gustavo'];
-const alunos5ano = alunos1.map(function(nomesENotas) {
-    nomesENotas = {
-        nome: nomesENotas,
-        nota: 5
-    }
-    return nomesENotas;
-})
-console.log(alunos5ano);
+const alunos = [
+    { nome: 'Murilo', nota: 5 },
+    { nome: 'André', nota: 6 },
+    { nome: 'Luciana', nota: 7 },
+    { nome: 'Gustavo', nota: 8 }
+];
 
-alunos5ano.push({
-    nome: 'Douglas',
-    nota: 8
-})
+alunos.push({nome: 'Douglas', nota: 10});
 
-const notaMaiorQ6 = alunos5ano.find(function(nota) {
-    return nota.nota == 8
-})
-console.log(notaMaiorQ6);
+const alunosNotas = alunos.filter(alunos => alunos.nota >= 7);
+console.log(alunosNotas);
 
-const nota8 = alunos5ano.filter(function(achandoNota8) {
-    return achandoNota8.nota === 8
-})
-console.log(nota8);
+
+
+// const alunos1 = ['Murilo', 'André', 'Luciana', 'Gustavo'];
+// const alunos5ano = alunos1.map(function(nomesENotas) {
+//     nomesENotas = {
+//         nome: nomesENotas,
+//         nota: 5
+//     }
+//     return nomesENotas;
+// })
+// console.log(alunos5ano);
+
+// alunos5ano.push({
+//     nome: 'Douglas',
+//     nota: 8
+// })
+
+// const notaMaiorQ6 = alunos5ano.find(function(nota) {
+//     return nota.nota >= 8
+// })
+// console.log(notaMaiorQ6);
+
+// const nota8 = alunos5ano.filter(function(achandoNota8) {
+//     return achandoNota8.nota == 8
+// })
+// console.log(nota8);
